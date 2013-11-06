08d3cb58d4c93ab12c40ca90737add83990b0254
Jeremy Zongker <jeremy@trilitech.com>
2013-11-06 13:02:32 -0600

    Moved to Pages model
    Implimented Jquery Mobile Pages and removed the ajax calls.

--- js/charitybets.js
+++ js/charitybets.js
@@ -1,13 +1,21 @@
 
 function showTab(tab)
 {
-    $('#appFooter').find('a').removeAttr('style');
-    $('#content').load('ajax/' + tab + '.html', function() {
-        $('#content').trigger('create');
-        $('#tab_' + tab).css('background-color', '#000000').css('background-image', 'none');
+    $('.appFooter').find('a').removeAttr('style');
+    $('#tab_' + tab).css('background-color', '#000000').css('background-image', 'none');
+}
+
+function init()
+{
+    $('#mybets').load('ajax/mybets.html', function(){
+        $('#allbets').load('ajax/allbets.html', function(){
+            $('#leaderboard').load('ajax/leaderboard.html', function() {
+                $('#container').trigger('create');
+            });
+        });
     });
 }
 
 $(function() {
-    showTab('leaderboard');
+    //init();
 });

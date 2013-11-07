37b999d556b45a248d92a20b302c79da96eeb001
Jeremy Zongker <jeremy@trilitech.com>
2013-11-06 22:12:28 -0600

    Added API call
    Added an call to the JSON API to load the initial app data and stored it
    in a global variable that's not yet used for anything.

--- js/charitybets.js
+++ js/charitybets.js
@@ -1,21 +1,14 @@
+var appData = {};
 
-function showTab(tab)
-{
-    $('.appFooter').find('a').removeAttr('style');
-    $('#tab_' + tab).css('background-color', '#000000').css('background-image', 'none');
-}
 
 function init()
 {
-    $('#mybets').load('ajax/mybets.html', function(){
-        $('#allbets').load('ajax/allbets.html', function(){
-            $('#leaderboard').load('ajax/leaderboard.html', function() {
-                $('#container').trigger('create');
-            });
-        });
+    $.getJSON( apiUrl + '/charitybets/?a=appData&k=qwerty', function( data ) {
+        console.log(data);
+        appData = data;
     });
 }
 
 $(function() {
-    //init();
+    init();
 });
--- js/pledgeapps.js
+++ js/pledgeapps.js
@@ -1,4 +1,4 @@
-var apiUrl = 'http://api.pledgeapps.com/';
+var apiUrl = 'http://192.168.8.100:3101';
 var userData = {};
 
 /*
